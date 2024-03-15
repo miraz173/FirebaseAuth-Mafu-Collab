13e543442f7e3b5df6b1cbf26c251db2139ffb34
Moshiur Rahman Naeem <68812831+miraz173@users.noreply.github.com>
2024-03-15 14:47:51 +0600

    Removed api keys and other critical data firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,8 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuI6qWnKp3ky_bqdl9Dxuocp4O7ggqMsI",
-  authDomain: "miraz-fa035.firebaseapp.com",
-  databaseURL: "https://miraz-fa035.firebaseio.com",
-  projectId: "miraz-fa035",
-  storageBucket: "miraz-fa035.appspot.com",
-  messagingSenderId: "130767717396",
-  appId: "1:130767717396:web:ff0bc1b023207d154bd301",
-  measurementId: "G-KFJ0P2LYKB",
+//removed, as security to prevent misuse of he storage
+  //copy from firebase config file firebase site
 };
 
 const app = initializeApp(firebaseConfig);
